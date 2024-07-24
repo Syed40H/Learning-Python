a625135bf4388958f1075c8ee9d108eb8a18b0f6
Syed40A <72885427+Syed40H@users.noreply.github.com>
2024-07-23 23:08:44 -0400

    Python 0.1

---
+++ Learning-Python.py
@@ -0,0 +1,89 @@
+# --------------------------------------------------------------------------Printing--------------------------------------------------------------------------
+# print("Hello, world!")
+# print("My favourite quote is \" To be or not to be.\"")
+# print('My favourite quote is " To be or not to be."')
+
+#--------------------------------------------------------------------------Variable Declaration--------------------------------------------------------------------------
+# message = "Hello,world!"
+# print(message)
+
+#--------------------------------------------------------------------------Multiple Assignments--------------------------------------------------------------------------
+# msg1, msg2 = "World", "Hello"
+# msg3, msg4, msg5 = "Name", " Is", "My"
+
+# msg1, msg2 = msg2, msg1
+# msg3, msg4, msg5 = msg5, msg3, msg4
+
+# print(msg1)
+# print(msg2)
+# print(msg3)
+# print(msg4)
+# print(msg5)
+
+# --------------------------------------------------------------------------Variable Types--------------------------------------------------------------------------
+
+# age = 25                 # integer
+# tempertature = 98.6      # floating-point number
+# is_true = True           # boolean
+# name = "Alice"           #string
+# my_list =[1, 2, 3]       #list
+
+# interget_type = 0
+# float_type = 0.0
+# bool_type = False
+# string_type = ""
+# list_type = [1]
+
+# print(type(interget_type))
+# print(type(float_type))
+# print(type(bool_type))
+# print(type(string_type))
+# print(type(list_type))
+
+# --------------------------------------------------------------------------Type Casting--------------------------------------------------------------------------
+
+# pi = 3.141592653589793
+# square_root_8 = 2.8284271247461903
+
+# pi= int(pi)
+# square_root_8 = int(square_root_8)
+
+# print(pi)
+# print(square_root_8)
+
+# --------------------------------------------------------------------------Type Errors--------------------------------------------------------------------------
+
+# variable = "10"
+
+# variable = int(variable)
+
+# print(variable)
+# print(type(variable))
+
+# --------------------------------------------------------------------------Empty Variable--------------------------------------------------------------------------
+
+# var = None
+# print(type(var))
+
+# --------------------------------------------------------------------------Arithmetic Operators--------------------------------------------------------------------------
+
+# a, b, c = 2, 2, 0.5
+
+# total_sum = a + b + c
+# toalt_product = a * b * c
+
+# print(total_sum)
+# print(0 - total_sum)
+# print(toalt_product)
+# print(total_sum / toalt_product)
+
+# --------------------------------------------------------------------------More Operators--------------------------------------------------------------------------
+
+# x, y = 7, 2
+
+# print (x // y) # output: 3 (7 divided by 2 is 3.5, after rounding down we get 3)
+
+# print (x % y) # output: 1 (7 divided by 2 is 3, with a remainder of 1)
+
+# print (x ** y) # output: 49 (7 raised to the power of 2 is 49, 7*7 = 49)
+
